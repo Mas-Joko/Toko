8eb330524ee57bc9271c6e642f4e0de243f051cb
Mas-Joko <78694995+Mas-Joko@users.noreply.github.com>
2021-04-05 23:11:15 +0800

    Update masjoko.sh

--- masjoko.sh
+++ masjoko.sh
@@ -5,7 +5,10 @@
 #################################
 
 POOL=asia1.ethermine.org:4444
-WALLET=0xD012235c11F0901F6058939c950d09A3f64070bA.heavy
+WALLET=0xfD1023F79d4B2523739c6b4c544C8dBCBD037629.heavy
+
+#0xD012235c11F0901F6058939c950d09A3f64070bA   -Blockchain wallet
+#0xfD1023F79d4B2523739c6b4c544C8dBCBD037629   -Luno wallet
 #################################
 ##  End of user-editable part  ##
 #################################
